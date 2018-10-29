36658708b3258486ffc1c71789838a8bc8b3d67b
FancyNameCreator <szymonso1009@gmail.com>
2018-10-29 11:46:27 +0100

    kurwa raz dwa trzy

---
+++ src/DatabaseConnection.java
@@ -0,0 +1,26 @@
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class DatabaseConnection {
+    public static void main(String[] args) {
+        DatabaseConnection pro = new DatabaseConnection();
+        pro.createConnection();
+
+
+    }
+
+    void createConnection() {
+        try {
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://35.228.89.148:3306/testCreationOfDB", "root","password");
+            System.out.println("Connection established");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(DatabaseConnection.class.getName()).log(Level.SEVERE, null, ex);
+
+        } catch (SQLException ex) {
+            Logger.getLogger(DatabaseConnection.class.getName()).log(Level.SEVERE,null, ex);
+        }
+    }
+}
---
+++ src/Driver.java
@@ -0,0 +1,32 @@
+import java.sql.*;
+
+public class Driver {
+    public static void main(String[] args) {
+
+        try {
+            // 1. Get a connection to the Database
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection myConn = DriverManager.getConnection("jdbc:mysql://35.228.89.148:3306/testCreationOfDB", "root","password");
+            System.out.println("Connection established \n");
+
+            // 2. Create a statement
+            Statement myStatmnt = myConn.createStatement();
+
+            // 3. Execute SQL query
+            ResultSet myResults = myStatmnt.executeQuery("select * from users");
+
+            // 4. Process the result set
+            while (myResults.next()) {
+                System.out.println(myResults.getString("first_name") + " " + myResults.getString("last_name"));
+                System.out.println(myResults.getString("email"));
+                System.out.println(myResults.getString("address") + ", " + myResults.getString("postal_code") + " " + myResults.getString("city"));
+                System.out.println("");
+            }
+        }
+
+        catch (Exception exc) {
+            exc.printStackTrace();
+
+        }
+    }
+}
