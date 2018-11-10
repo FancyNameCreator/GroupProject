d58e2f341a08df0eec412d9c9f366c0324ab15cd
Alicja <alicjaharaszczuk@gmail.com>
2018-11-10 13:53:27 +0100

    events: inserted boundaries for input events' dates, taking leap years into consideration

--- src/Database/EventsMethodsClass.java
+++ src/Database/EventsMethodsClass.java
@@ -51,32 +51,36 @@ public class EventsMethodsClass {
         }
         public static void updateEvent(int id) {
         Scanner input = new Scanner(System.in);
-            System.out.println("Choose what do you want to do:\n0.Exit\n1.Change event name\n2.Change event date\n3.Change event location\n4.Change event description\n5. Change event category");
+            System.out.println("Choose what do you want to do:\n0.Exit\n1.Change event name\n2.Change event date\n3.Change event location\n4.Change event description\n5. Change event category\n6. Return to menu");
             eventChoice = 1;
             while (eventChoice!=0) {
                 eventChoice = input.nextInt();
                 switch (eventChoice) {
                     case 1:
                         changeEventName(id);
-                        System.out.println("If you want to change something more insert one of displayed numbers:\n0.Exit\n1.Change event name\n2.Change event date\n3.Change event location\n4.Change event description\n5. Change event category");
+                        System.out.println("If you want to change something more insert one of displayed numbers:\n0.Exit\n1.Change event name\n2.Change event date\n3.Change event location\n4.Change event description\n5. Change event category\n6. Return to menu");
                         break;
                     case 2:
                         changeEventDate(id);
-                        System.out.println("If you want to change something more insert one of displayed numbers:\n0.Exit\n1.Change event name\n2.Change event date\n3.Change event location\n4.Change event description\n5. Change event category");
+                        System.out.println("If you want to change something more insert one of displayed numbers:\n0.Exit\n1.Change event name\n2.Change event date\n3.Change event location\n4.Change event description\n5. Change event category\n6. Return to menu");
                         break;
                     case 3:
                         changeEventLocation(id);
-                        System.out.println("If you want to change something more insert one of displayed numbers:\n0.Exit\n1.Change event name\n2.Change event date\n3.Change event location\n4.Change event description\n5. Change event category");
+                        System.out.println("If you want to change something more insert one of displayed numbers:\n0.Exit\n1.Change event name\n2.Change event date\n3.Change event location\n4.Change event description\n5. Change event category\n6. Return to menu");
                         break;
                     case 4:
                         changeEventDescription(id);
-                        System.out.println("If you want to change something more insert one of displayed numbers:\n0.Exit\n1.Change event name\n2.Change event date\n3.Change event location\n4.Change event description\n5. Change event category");
+                        System.out.println("If you want to change something more insert one of displayed numbers:\n0.Exit\n1.Change event name\n2.Change event date\n3.Change event location\n4.Change event description\n5. Change event category\n6. Return to menu");
                         break;
                     case 5:
                         changeEventCategory(id);
-                        System.out.println("If you want to change something more insert one of displayed numbers:\n0.Exit\n1.Change event name\n2.Change event date\n3.Change event location\n4.Change event description\n5. Change event category");
+                        System.out.println("If you want to change something more insert one of displayed numbers:\n0.Exit\n1.Change event name\n2.Change event date\n3.Change event location\n4.Change event description\n5. Change event category\n6. Return to menu");
+                        break;
                     case 0:
-                        System.out.println("You are about to exit this page");
+                        System.out.println("You are about to exit this page.");
+                        break;
+                    case 6:
+                        menuEvents();
                         break;
                     default:
                         System.out.println("Number was not recognized. Please, insert one of displayed above numbers.");
@@ -85,44 +89,117 @@ public class EventsMethodsClass {
         }
         public static void addEvent() {
             Scanner input = new Scanner(System.in);
+
             System.out.println("Start creating your event!");
+
             System.out.print("Add event name: ");
             String eventName = input.nextLine();
-            System.out.print("Add the date\n ");
-            System.out.print("Year (insert two last digits of the year): ");
-            String eventDateYear = input.nextLine();
-            System.out.print("\nMonth(insert a number): ");
-            String eventDateMonth = input.nextLine();
-            System.out.print("\nDay(insert a number): ");
-            String eventDateDay = input.nextLine();
-            String eventDate = eventDateYear + "." + eventDateMonth + "." + eventDateDay;
-            System.out.print("Add event location: ");
-            String eventLocation = input.nextLine();
-            System.out.print("Add event description: ");
-            String eventDescription = input.nextLine();
-            System.out.println("Add event category from the ones stated below:\n1. Food event\n2. Clubbing\n3. Music event\n4. Just meeting\n5. Sport match\n6.Hobby event");
-            int eventCategory = input.nextInt();
-            String eventCategoryString  = "";
 
-            switch (eventCategory) {
-                case 1:
-                    eventCategoryString = "Food event";
+            System.out.println("Add event description: ");
+            String eventDescription = input.nextLine();
+
+            System.out.println("Add event location: ");
+            String eventLocation = input.nextLine();
+
+            System.out.print("Add the date\n ");
+
+            System.out.print("Year (insert all 4 digits): ");
+            int eventDateYear = input.nextInt();
+            System.out.print("\nMonth(insert a number): ");
+            int eventDateMonth1 = 1;
+            int eventDateMonth = 1;
+            while (eventDateMonth1!=0) {
+                eventDateMonth1 = input.nextInt();
+                if (eventDateMonth1 >= 1 && eventDateMonth1 <= 12) {
+                    eventDateMonth = eventDateMonth1;
                     break;
-                case 2:
-                    eventCategoryString = "Clubbing";
+                } else {
+                    System.out.println("Wrong number of the month. Try again.");
+                }
+            }
+            System.out.print("\nDay(insert a number): ");
+            int eventDateDay1 = 1;
+            int eventDateDay = 1;
+            if (eventDateYear%4==0&&eventDateMonth==2)
+            {
+                while (eventDateDay1!=0) {
+                    eventDateDay1 = input.nextInt();
+                    if (eventDateDay1 >= 1 && eventDateDay1 <= 29) {
+                        eventDateDay = eventDateDay1;
+                        break;
+                    }
+                    else {
+                        System.out.println("Wrong number of the day. Try again.");
+                    }
+                }
+            }
+            else {
+                if (eventDateMonth == 1 || eventDateMonth == 3 || eventDateMonth == 5 || eventDateMonth == 7 || eventDateMonth == 8 || eventDateMonth == 10 || eventDateMonth == 12) {
+                    while (eventDateDay1 != 0) {
+                        eventDateDay1 = input.nextInt();
+                        if (eventDateDay1 >= 1 && eventDateDay1 <= 31) {
+                            eventDateDay = eventDateDay1;
+                            break;
+                        } else {
+                            System.out.println("Wrong number of the day. Try again.");
+                        }
+                    }
+                } else if (eventDateMonth == 4 || eventDateMonth == 6 || eventDateMonth == 9 || eventDateMonth == 11) {
+                    while (eventDateDay1 != 0) {
+                        eventDateDay1 = input.nextInt();
+                        if (eventDateDay1 >= 1 && eventDateDay1 <= 30) {
+                            eventDateDay = eventDateDay1;
+                            break;
+                        } else {
+                            System.out.println("Wrong number of the day. Try again.");
+                        }
+                    }
+                } else if (eventDateMonth == 2) {
+                    while (eventDateDay1 != 0) {
+                        eventDateDay1 = input.nextInt();
+                        if (eventDateDay1 >= 1 && eventDateDay1 <= 28) {
+                            eventDateDay = eventDateDay1;
+                            break;
+                        } else {
+                            System.out.println("Wrong number of the day. Try again.");
+                        }
+                    }
+                }
+            }
+
+            String eventDate = eventDateYear + "." + eventDateMonth + "." + eventDateDay;
+
+            System.out.println("Add event category from the ones stated below:\n1. Food event\n2. Clubbing\n3. Music event\n4. Just meeting\n5. Sport match\n6.Hobby event");
+            int eventCategory = 1;
+            String eventCategoryString  = "";
+            while (eventCategory!=0) {
+                eventCategory = input.nextInt();
+                switch (eventCategory) {
+                    case 1:
+                        eventCategoryString = "Food event";
+                        break;
+                    case 2:
+                        eventCategoryString = "Clubbing";
+                        break;
+                    case 3:
+                        eventCategoryString = "Music event";
+                        break;
+                    case 4:
+                        eventCategoryString = "Just meeting";
+                        break;
+                    case 5:
+                        eventCategoryString = "Sport match";
+                        break;
+                    case 6:
+                        eventCategoryString = "Hobby event";
+                        break;
+                    default:
+                        System.out.println("Category was not recognized. Try again.");
+                        break;
+                }
+                if (eventCategory==1||eventCategory==2||eventCategory==3||eventCategory==4||eventCategory==5||eventCategory==6) {
                     break;
-                case 3:
-                    eventCategoryString = "Music event";
-                    break;
-                case 4:
-                    eventCategoryString = "Just meeting";
-                    break;
-                case 5:
-                    eventCategoryString = "Sport match";
-                    break;
-                case 6:
-                    eventCategoryString = "Hobby event";
-                    break;
+                }
             }
 
 
@@ -143,6 +220,16 @@ public class EventsMethodsClass {
             catch (SQLException e) {
                 e.printStackTrace();
             }
+
+            System.out.println("If you want to return to the menu, press 1, otherwise press any other number.");
+            int answer = input.nextInt();
+            if (answer==1) {
+                menuEvents();
+            }
+            else {
+                System.out.println("You have left the page.");
+            };
+
         }
         public static void removeEvent(int id) {
             System.out.println("Enter event id to delete: ");
@@ -161,6 +248,14 @@ public class EventsMethodsClass {
             } catch (SQLException e) {
                 e.printStackTrace();
             }
+            System.out.println("If you want to return to the menu, press 1, otherwise press anything.");
+            int answer = input.nextInt();
+            if (answer==1) {
+                menuEvents();
+            }
+            else {
+                System.out.println();
+            };
 
         }
 
@@ -185,13 +280,71 @@ public class EventsMethodsClass {
 
         }
         public static void changeEventDate(int id) {
-            System.out.println("Enter new event date:\n Year (insert two last digits of the year):  ");
+            System.out.println("Enter new event date:\n Year (insert 4 digits):  ");
             Scanner input = new Scanner(System.in);
             int eventDateYear = input.nextInt();
             System.out.println("\n Month(insert number): ");
-            int eventDateMonth = input.nextInt();
+            int eventDateMonth1 = 1;
+            int eventDateMonth = 1;
+            while (eventDateMonth1!=0) {
+                eventDateMonth1 = input.nextInt();
+                if (eventDateMonth1 >= 1 && eventDateMonth1 <= 12) {
+                    eventDateMonth = eventDateMonth1;
+                    break;
+                } else {
+                    System.out.println("Wrong number of the month. Try again.");
+                }
+            }
             System.out.println("\n Day (insert number): ");
-            int eventDateDay = input.nextInt();
+            int eventDateDay1 = 1;
+            int eventDateDay = 1;
+            if (eventDateYear%4==0&&eventDateMonth==2)
+            {
+                while (eventDateDay1!=0) {
+                    eventDateDay1 = input.nextInt();
+                    if (eventDateDay1 >= 1 && eventDateDay1 <= 29) {
+                        eventDateDay = eventDateDay1;
+                        break;
+                    }
+                    else {
+                        System.out.println("Wrong number of the day. Try again.");
+                    }
+                }
+            }
+            else {
+                if (eventDateMonth == 1 || eventDateMonth == 3 || eventDateMonth == 5 || eventDateMonth == 7 || eventDateMonth == 8 || eventDateMonth == 10 || eventDateMonth == 12) {
+                    while (eventDateDay1 != 0) {
+                        eventDateDay1 = input.nextInt();
+                        if (eventDateDay1 >= 1 && eventDateDay1 <= 31) {
+                            eventDateDay = eventDateDay1;
+                            break;
+                        } else {
+                            System.out.println("Wrong number of the day. Try again.");
+                        }
+                    }
+                } else if (eventDateMonth == 4 || eventDateMonth == 6 || eventDateMonth == 9 || eventDateMonth == 11) {
+                    while (eventDateDay1 != 0) {
+                        eventDateDay1 = input.nextInt();
+                        if (eventDateDay1 >= 1 && eventDateDay1 <= 30) {
+                            eventDateDay = eventDateDay1;
+                            break;
+                        } else {
+                            System.out.println("Wrong number of the day. Try again.");
+                        }
+                    }
+                } else if (eventDateMonth == 2) {
+                    while (eventDateDay1 != 0) {
+                        eventDateDay1 = input.nextInt();
+                        if (eventDateDay1 >= 1 && eventDateDay1 <= 28) {
+                            eventDateDay = eventDateDay1;
+                            break;
+                        } else {
+                            System.out.println("Wrong number of the day. Try again.");
+                        }
+                    }
+                }
+            }
+
             String eventDate = eventDateYear + "." + eventDateMonth + "." + eventDateDay;
             Connection conn = null;
             Statement stmt = null;
