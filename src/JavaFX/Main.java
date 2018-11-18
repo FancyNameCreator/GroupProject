00004898548b3ac6673fd14a130e9115717d5843
FancyNameCreator <szymonso1009@gmail.com>
2018-11-18 15:02:34 +0100

    One more update for today, I fixed the size of the windows, now all of them are made in 900x600 standard

--- src/JavaFX/Main.java
+++ src/JavaFX/Main.java
@@ -25,7 +25,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/resources/sample.fxml"));
         primaryStage.setTitle("Hanger");
-        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.setScene(new Scene(root, 900, 600));
         window = primaryStage;
         window.show();
     }
