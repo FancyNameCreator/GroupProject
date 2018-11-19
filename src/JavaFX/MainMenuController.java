74b5053fa32316d77396dd6d0d1cf99827b7edae
Julius Augustus <juliusjoller@gmail.com>
2018-11-19 10:38:58 +0100

    Add files via upload

---
+++ src/JavaFX/MainMenuController.java
@@ -0,0 +1,47 @@
+package JavaFX;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
+
+public class MainMenuController {
+    @FXML
+    private AnchorPane mainPane;
+
+    @FXML
+    private void goToProfile(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/resources/profilePage.fxml"));
+        BorderPane pane = loader.load();
+        mainPane.getChildren().setAll(pane);
+    }
+
+    @FXML
+    private void goToEvents(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/resources/events.fxml"));
+        AnchorPane pane = loader.load();
+        mainPane.getChildren().setAll(pane);
+    }
+
+    @FXML
+    private void goToFriends(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/resources/friendsPage.fxml"));
+        AnchorPane pane = loader.load();
+
+        mainPane.getChildren().setAll(pane);
+    }
+
+    @FXML
+    private void goToChat(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/resources/profilePage.fxml"));
+        AnchorPane pane = loader.load();
+        mainPane.getChildren().setAll(pane);
+    }
+}
