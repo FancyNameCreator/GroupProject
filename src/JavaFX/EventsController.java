34bb1b746ff4b75974ab926396741b4dde2bed97
FancyNameCreator <szymonso1009@gmail.com>
2018-11-20 09:55:48 +0100

    some fxmls and classes added

--- src/JavaFX/EventsController.java
+++ src/JavaFX/EventsController.java
@@ -5,10 +5,10 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 public class EventsController {
     private ObservableList<String> usersChoice = FXCollections.observableArrayList("My events(attended/to attend and created)", "Discover(search by category and attend)", "Events my friend created", "Create event");
@@ -28,23 +28,32 @@ public class EventsController {
     }
 
     @FXML
-    private void changeLabel() throws IOException{
+    private void changeLabel() throws IOException/*, InterruptedException*/{
 
         switch (choicebox.getValue()) {
+
+            //ask sokol about it
+
             case ("My events(attended/to attend and created)"):
                 labelOfUserChoice.setText("You have chosen: " + choicebox.getValue());
+                choicebox.setDisable(true);
+                //TimeUnit.SECONDS.sleep(5);
                 loadMyEvents();
                 break;
             case ("Discover(search by category and attend)"):
                 labelOfUserChoice.setText("You have chosen: " + choicebox.getValue());
+                choicebox.setDisable(true);
+                //TimeUnit.SECONDS.sleep(1);
                 loadDiscover();
                 break;
             case ("Events my friends are going/created"):
                 labelOfUserChoice.setText("You have chosen: " + choicebox.getValue());
+                //TimeUnit.SECONDS.sleep(1);
                 loadFriendsEvents();
                 break;
             case ("Create event"):
                 labelOfUserChoice.setText("You have chosen: " + choicebox.getValue());
+                //TimeUnit.SECONDS.sleep(1);
                 loadCreateEvent();
                 break;
             default:
