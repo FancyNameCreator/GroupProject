7ef9c1d216e507c2481c38e97249ea82a2557480
FancyNameCreator <szymonso1009@gmail.com>
2018-11-21 14:23:16 +0100

    discover under construction

--- src/JavaFX/DiscoverController.java
+++ src/JavaFX/DiscoverController.java
@@ -1,5 +1,7 @@
 package JavaFX;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
@@ -8,32 +10,76 @@ import javafx.scene.control.TableView;
 import javafx.scene.layout.BorderPane;
 
 public class DiscoverController {
+    private ObservableList<String> usersChoice = FXCollections.observableArrayList(
+            "Food event","Clubbing","Music event","Just meeting","Sport match","Hobby event");
 
-    @FXML
-    private BorderPane borderPaneDiscover;
+    @FXML private BorderPane borderPaneDiscover;
 
-    @FXML
-    private ChoiceBox<?> choiceBoxDiscover;
+    @FXML private ChoiceBox<String> choiceBoxDiscover;
 
-    @FXML
-    private TableView<?> tableViewDiscover;
+    @FXML private TableView<Event> tableViewDiscover;
 
-    @FXML
-    private TableColumn<?, ?> eventNameColumnDiscover;
+    @FXML private TableColumn<Event, String> eventNameColumnDiscover;
 
-    @FXML
-    private TableColumn<?, ?> eventLocationColumnDiscover;
+    @FXML private TableColumn<Event, String> eventLocationColumnDiscover;
 
-    @FXML
-    private TableColumn<?, ?> eventDateColumnDiscover;
+    @FXML private TableColumn<Event, String> eventDateColumnDiscover;
 
-    @FXML
-    private TableColumn<?, ?> eventCategoryColumnCategory;
+    @FXML private TableColumn<Event, String> eventCategoryColumnCategory;
 
-    @FXML
-    private TableColumn<?, ?> eventCreatorColumnDiscover;
+    @FXML private TableColumn<Event, String> eventCreatorColumnDiscover;
 
     @FXML
     private Button buttonGo;
+
+    @FXML
+    private void showTableDiscover(){
+        switch (choiceBoxDiscover.getValue()) {
+            case ("Food event"):
+                loadFoodEvents();
+                break;
+            case ("Clubbing") :
+                loadClubbingEvents();
+                break;
+            case ("Music event") :
+                loadMusicEvents();
+                break;
+            case ("Just meeting") :
+                loadMeetingsEvents();
+                break;
+            case ("Sport match") :
+                loadSportsEvents();
+                break;
+            case ("Hobby event") :
+                loadHobbyEvents();
+                break;
+            default:
+                System.out.println("YOU'VE BETTER LEARN HOW TO USE JFX!");
+        }
+    }
+
+    private void loadFoodEvents(){
+
+    }
+
+    private void loadClubbingEvents(){
+
+    }
+
+    private void loadMusicEvents(){
+
+    }
+
+    private void loadMeetingsEvents(){
+
+    }
+
+    private void loadSportsEvents(){
+
+    }
+
+    private void loadHobbyEvents(){
+
+    }
 
 }
