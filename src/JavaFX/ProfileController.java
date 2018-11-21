6ba58ca1cdb681df888876ce920fc908b393e444
Rami <43989050+EvilComrade@users.noreply.github.com>
2018-11-21 13:47:20 +0100

    Changed size

--- src/JavaFX/ProfileController.java
+++ src/JavaFX/ProfileController.java
@@ -188,7 +188,7 @@ public class ProfileController{
     private void goToMainMenu(ActionEvent actionEvent) throws IOException {
         FXMLLoader loader = new FXMLLoader();
         loader.setLocation(getClass().getResource("/resources/mainMenu.fxml"));
-        AnchorPane pane = (AnchorPane) loader.load();
+        BorderPane pane = (BorderPane) loader.load();
         profilePane.getChildren().setAll(pane);
     }
 
