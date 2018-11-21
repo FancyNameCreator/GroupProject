c303a85c583e7d2c8daed18b87924dd266ed28bb
Alicja <alicjaharaszczuk@gmail.com>
2018-11-21 13:52:24 +0100

    database is loading

--- src/JavaFX/DiscoverController.java
+++ src/JavaFX/DiscoverController.java
@@ -1,4 +1,39 @@
 package JavaFX;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.BorderPane;
+
 public class DiscoverController {
+
+    @FXML
+    private BorderPane borderPaneDiscover;
+
+    @FXML
+    private ChoiceBox<?> choiceBoxDiscover;
+
+    @FXML
+    private TableView<?> tableViewDiscover;
+
+    @FXML
+    private TableColumn<?, ?> eventNameColumnDiscover;
+
+    @FXML
+    private TableColumn<?, ?> eventLocationColumnDiscover;
+
+    @FXML
+    private TableColumn<?, ?> eventDateColumnDiscover;
+
+    @FXML
+    private TableColumn<?, ?> eventCategoryColumnCategory;
+
+    @FXML
+    private TableColumn<?, ?> eventCreatorColumnDiscover;
+
+    @FXML
+    private Button buttonGo;
+
 }
