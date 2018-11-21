996cb2211d864010eb0a546d041620e76c9f346b
FancyNameCreator <szymonso1009@gmail.com>
2018-11-21 16:02:52 +0100

    Hi there, my and Alicia's final update for today. We have done alpha version of events, now you can: - see what events you are attending/created - visit discover page - create an event
    AMAZING JOB TODAY, GUYS!

--- src/JavaFX/CreateEventController.java
+++ src/JavaFX/CreateEventController.java
@@ -2,11 +2,33 @@ package JavaFX;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
 
 import java.sql.SQLException;
 
 public class CreateEventController {
+
+    @FXML
+    private TextField textFieldName;
+
+    @FXML
+    private TextField textFieldLocation;
+
+    @FXML
+    private TextField textFieldDate;
+
+    @FXML
+    private TextField textFieldDescription;
+
+    @FXML
+    private ChoiceBox<String> choiceBoxCreate;
+
+    @FXML
+    private Button buttonCreate;
 
     String nameOfEvent;
     String locationOfEvent;
@@ -24,19 +46,26 @@ public class CreateEventController {
         choiceBoxCreate.setItems(category);
     }
 
-    private void createEvent(){
+    @FXML
+    private void createEvent(ActionEvent ae){
         getData();
         sendData();
     }
 
     private void getData(){
-
-    }
+        nameOfEvent = textFieldName.getText();
+        locationOfEvent = textFieldLocation.getText();
+        dateOfEvent = textFieldDate.getText();
+        descriptionOfEvent = textFieldDescription.getText();
+        participantsOfEvent = "";
+        creatorOfEvent = Main.getEmailIN();
+        categoryOfEvent = choiceBoxCreate.getValue();
+        }
 
     private void sendData(){
         try{
-            String sql = "insert into users "
-                    + "(first_name, last_name, email, password, address, postal_code, city, country, DoB)"
+            String sql = "insert into events "
+                    + "(event_name,event_date,event_location,event_description,event_category,participants,creator)"
                     + "values ('" + nameOfEvent + "','" + dateOfEvent + "','" + locationOfEvent + "','" + descriptionOfEvent + "','" + categoryOfEvent + "','" + participantsOfEvent + "', '" + creatorOfEvent + "')";
             // executing MySQL command that value is stored in sql variable
             Main.stmt.executeUpdate(sql);
